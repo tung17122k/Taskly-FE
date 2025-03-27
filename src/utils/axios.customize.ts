db45365ea1f04fc3457a72d47f62936ff8310257
tung17122k <sanking1712@gmail.com>
2025-03-27 21:43:09 +0700

    axios interceptor

---
+++ src/utils/axios.customize.ts
@@ -0,0 +1,33 @@
+// https://github.com/axios/axios#config-defaults
+//https://github.com/axios/axios?tab=readme-ov-file#interceptors
+
+import axios, { AxiosInstance, InternalAxiosRequestConfig, AxiosResponse, AxiosError } from 'axios';
+
+
+const instance: AxiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_BACKEND_URL as string
+});
+
+
+instance.interceptors.request.use(
+    (config: InternalAxiosRequestConfig): InternalAxiosRequestConfig => {
+        return config;
+    },
+    (error: AxiosError): Promise<AxiosError> => {
+        return Promise.reject(error);
+    }
+);
+
+
+instance.interceptors.response.use(
+    (response: AxiosResponse): AxiosResponse => {
+
+        return response;
+    },
+    (error: AxiosError): Promise<AxiosError> => {
+
+        return Promise.reject(error);
+    }
+);
+
+export default instance;
